f75df018d0675508040beec0a29dc36b0aca04bb
MarcoBusto <msbusto@gmail.com>
2024-01-22 21:04:20 -0300

    greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Navigate, Routes,Route, Outlet } from 'react-router-dom'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-
+import ItemListContainter from './components/ItemListContainer/ItemListContainer'
+import Greeting from './components/greeting'
 
 
 //importamos la nav bar //
@@ -11,18 +11,19 @@ import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <>
+    <>     
     <Container id='main'>
-      <Navbar/>      
+      <Navbar/>
       <div id='contenidos'>
+      <Greeting name="Tienda Demonia"/> 
       <h1>Tienda de Productos</h1>
-      <ItemListContainer
+      <ItemListContainter
         nombre="Mate premium"
         precio={15000}
         descripcion="Mate térmico de excelente calidad con una bombilla con detalles muy delicados"
         imagen="src/img/matePremium.jpg"
       />
-      <ItemListContainer
+      <ItemListContainter
         nombre="Combo 1 (mate,vaso y botella térmica)"
         precio={25000}
         descripcion="Un Combo que se ajusta a todo tu día"
---
+++ src/components/greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Greeting({ name }) {
+    return (
+      <div>
+        <h1>Hola, {name}!</h1>
+      </div>
+    );
+  }
+export default Greeting
